cf7d8e9577fc5623b19a98c36eb2fc0583783bcf
Tziantopoulos Alexis (tzianale) <tzianale@students.zhaw.ch>
2024-04-04 09:16:36 +0200

    Update navigation and add text to home page

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -1,23 +1,20 @@
-import Particles from '@/components/Particles';
-
 export default function Home() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={300} />
-      <nav className="absolute top-0 left-0 right-0 py-8 z-10 backdrop-blur-sm  ">
-        <div className="container mx-auto  ">
+  return ( 
+    <div className="flex flex-col min-h-screen bg-gradient-to-b from-gray-900 to-gray-800">
+      <nav className="absolute top-0 left-0 right-0 py-8 z-10 backdrop-blur-sm">
+        <div className="container mx-auto">
           <div className="flex items-center justify-between">
             <div className="text-gray-300 font-bold pl-10 text-2xl">Solar System Simulator</div>
             <div>
-              <a
-                href="page.tsx"
-                className="relative inline-block text-gray-300 font-semibold px-4 mx-4 py-1.5 border-b-2 border-gray-300 "
+            <a
+                href="/"
+                className="group relative inline-block text-gray-300 font-semibold mx-4 px-4 py-2 rounded-2xl transition duration-300 before:absolute before:left-1/2 before:right-1/2 before:bottom-0 before:h-0.5 before:bg-gray-300 before:transition-all before:duration-300 hover:before:left-0 hover:before:right-0"
               >
                 Home
               </a>
               <a
                 href="/tutorial"
-                className="group relative inline-block text-gray-300 font-semibold mx-4 px-4 py-2 rounded-2xl transition duration-300 before:absolute before:left-1/2 before:right-1/2 before:bottom-0 before:h-0.5 before:bg-gray-300 before:transition-all before:duration-300 hover:before:left-0 hover:before:right-0"
+                className="relative inline-block text-gray-300 font-semibold px-4 mx-4 py-1.5 border-b-2 border-gray-300"
               >
                 Tutorial
               </a>
@@ -38,11 +35,12 @@ export default function Home() {
         </div>
       </nav>
       <main className="relative flex-1 flex flex-col items-center justify-center z-0">
-        
-        <div className="absolute top-20 text-white text-4xl mt-12 font-semibold z-10">
-          Explore our Solar System
+        <div className="text-white text-4xl mt-12 font-semibold">
+          Welcome to the Tutorial Page
         </div>
-
+        <p className="text-gray-300 mt-4 text-lg max-w-3xl text-center">
+          This is where you can find all the information you need to get started with our Solar System Simulator.
+        </p>
       </main>
     </div>
   );
