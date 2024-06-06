2a920c83f047aa1a7355e1330742d55b728b50ba
Tziantopoulos Alexis (tzianale) <tzianale@students.zhaw.ch>
2024-06-06 11:36:52 +0200

    chore: Update UI Components to improve clarity and formatting

--- src/app/tutorial/components/CoreComponents/uicomponents.tsx
+++ src/app/tutorial/components/CoreComponents/uicomponents.tsx
@@ -4,22 +4,44 @@ const CGUIComponents: React.FC = () => {
       <h2 className="text-2xl font-semibold mb-4">UI Components </h2>
 
       <div className=" rounded-lg p-6  mb-6">
-         <p className="mb-4">
+        <p className="mb-4">
           The following image depicts the starting state of the simulation. The program has four
-          interactable elements: <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">Speed Slider</span> , <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">Play/Pause button</span> , <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">Escape button</span> , as
-          well as a third button on the lower part of the screen with an <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">Arrow</span>  pointing upwards.{' '}
+          interactable elements:{' '}
+          <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">
+            Speed Slider
+          </span>{' '}
+          ,{' '}
+          <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">
+            Play/Pause button
+          </span>{' '}
+          ,{' '}
+          <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">
+            Escape button
+          </span>{' '}
+          , as well as a third button on the lower part of the screen with an{' '}
+          <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">
+            Arrow
+          </span>{' '}
+          pointing upwards.{' '}
         </p>
         <img src="/bild1.png" alt="Next.js Logo" className="mb-4" />
         <ul className="list-disc pl-6 mb-6">
           <li className="mb-2">
-            Simulation speed slider: You can use the <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">Speed Slider</span>  to increase the time
-            steps, which allows you to see planetary movements that would normally take years in
-            just seconds. – More details about the simulation speed will follow in the “Time
-            Controls” chapter –
+            Simulation speed slider: You can use the{' '}
+            <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">
+              Speed Slider
+            </span>{' '}
+            to increase the time steps, which allows you to see planetary movements that would
+            normally take years in just seconds. – More details about the simulation speed will
+            follow in the “Time Controls” chapter –
           </li>
           <li className="mb-2">
-            Play / Pause button: Clicking the <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">Play/Pause button</span>  allows you to pause or resume the
-            simulation, useful for taking screenshots or examining planetary positions closely.
+            Play / Pause button: Clicking the{' '}
+            <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">
+              Play/Pause button
+            </span>{' '}
+            allows you to pause or resume the simulation, useful for taking screenshots or examining
+            planetary positions closely.
           </li>
           <li className="mb-2">
             Forward button: The celestial body will move normally, and you can explore the future.
@@ -28,12 +50,20 @@ const CGUIComponents: React.FC = () => {
             Backward button: The celestial body will move in reverse, and you can explore the past.
           </li>
           <li className="mb-2">
-            Arrow button: Click the <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">Arrow button</span>  to toggle a list of planets from the bottom of the
-            screen. Click again to retract the list.
+            Arrow button: Click the{' '}
+            <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">
+              Arrow button
+            </span>{' '}
+            to toggle a list of planets from the bottom of the screen. Click again to retract the
+            list.
           </li>
           <li className="mb-2">
-            Escape button: Clicking the <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">Escape button</span>  stops the simulation and takes you back to the
-            main menu, where you can quit the program or start a new simulation.
+            Escape button: Clicking the{' '}
+            <span className=" bg-white text-slate-900 p-0.5 px-1.5 rounded-md shadow-lg font-bold ">
+              Escape button
+            </span>{' '}
+            stops the simulation and takes you back to the main menu, where you can quit the program
+            or start a new simulation.
           </li>
         </ul>
 
@@ -46,8 +76,8 @@ const CGUIComponents: React.FC = () => {
         </p>
         <ol className="list-disc pl-6 mb-6">
           <li className="mb-2">
-            Single Left Click: will open an info tab with details about the specific planet
-            (details about the Planet Info Tab will be explained in the following chapter)
+            Single Left Click: will open an info tab with details about the specific planet (details
+            about the Planet Info Tab will be explained in the following chapter)
           </li>
           <li className="mb-2">Two Clicks: will move the camera to focus on the planet.</li>
         </ol>
